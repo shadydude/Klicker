ab5c199e1a2f19c67b57d6631c82166a9c6bf451
Shayden <shayden@shaydens-mac.local>
2019-07-27 09:30:52 +0800

    Initial Commit

---
+++ Klicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Klicker
+//
+//  Created by Shayden Wong on 27/7/19.
+//  Copyright © 2019 Swift Accelerator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
