f54417df5a871619525f9855aa094cb9696cd706
Shayden <shayden@shaydens-mac.local>
2019-07-27 09:46:51 +0800

    STYLE: Design UI for application

---
+++ Klicker/Storyboards/Main/Clicker/ClickerViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ClickerViewController: UIViewController {
+    
+    // MARK
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
